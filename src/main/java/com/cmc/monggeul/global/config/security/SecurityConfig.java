3320322340fd9b4ed561ec3ba6a456ca69936549
JjungminLee <julie0964@naver.com>
2023-02-17 08:23:32 +0900

    fix:rds

--- src/main/java/com/cmc/monggeul/global/config/security/SecurityConfig.java
+++ src/main/java/com/cmc/monggeul/global/config/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
+    //
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
