e0675c8df02fcb63eaad98ce223b3b43562d3053
JjungminLee <julie0964@naver.com>
2023-03-25 10:24:03 +0900

    fix:category 조회 수정

--- src/main/java/com/cmc/monggeul/domain/diary/repository/CategoryRepository.java
+++ src/main/java/com/cmc/monggeul/domain/diary/repository/CategoryRepository.java
@@ -16,19 +16,19 @@ public interface CategoryRepository extends JpaRepository<Category,String> {
 
     Category findByCode(String code);
 
-    @Query(value = "select * from Category as c where (c.code like 'SON_MOM') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findMomSonCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'DAU_MOM') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findMomDauCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'SON_DAD') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findDadSonCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'DAU_DAD') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findDadDauCategory();
 
@@ -36,15 +36,15 @@ public interface CategoryRepository extends JpaRepository<Category,String> {
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findDauMomCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'DAD_DAU') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findDauDadCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'MOM_SON') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findSonMomCategory();
 
-    @Query(value = "select * from Category as c where (c.code like 'DAD_SON') or (c.code like 'YOUNG')" +
+    @Query(value = "select * from Category as c where (c.code like 'MOM_DAU') or (c.code like 'YOUNG')" +
             "or (c.code like 'REMEMBER') or (c.code like 'THINK')",nativeQuery = true)
     List<Category> findSonDadCategory();
 
